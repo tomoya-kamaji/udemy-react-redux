6b31c23515072731901584bbdc7c9ebac3013fbd
tomoyakamaji <tomoya.kamaji@gmail.com>
2020-06-13 21:37:17 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
-function App() {
+// class App extends Component {
+//   render() {
+//       return(
+//         <React.Fragment>
+//           <label htmlFor="bar">bar</label>
+//           <input type="text" onChange={() => {console.log("I am clicked!")}}/>
+//         </React.Fragment>
+//       )
+//   }
+// }
+
+const App = () => {
+  const profiles = [
+    {name:"kamaji",age:25},
+    {name:"sakura",age:22},
+    {name:"NonName",age:5}
+  ]
+  
+  
   return (
-    <div><h1>helloworld</h1></div>
-  );
+    <div>
+      {
+        profiles.map((profile) => {
+          return <User name={profile.name} age={profile.age}/>
+        })
+      }
+    </div>  
+  )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name} ! {props.age}years old</div>
 }
 
 export default App;
