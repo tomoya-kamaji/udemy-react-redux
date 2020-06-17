e89950e21d60631a15cfca6ea5d1dc4c13e9f82c
tomoyakamaji <tomoya.kamaji@gmail.com>
2020-06-17 22:16:56 +0900

    implement events

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom' 
+import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
 
-// import { postEvent } from '../actions'
+import { postEvent } from '../actions'
 
 class EventsNew extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field
+
     return (
-      // React.Fragmentはdivの代わりjsxは1つのタグで囲わなくてはいけない
-      <React.Fragment>
-        <div>new</div>
-      </React.Fragment>
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    )
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values)
+    this.props.history.push('/')
+  }
+
+  render() {
+    const { handleSubmit, pristine, submitting } = this.props
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+
+        <div>
+          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <Link to="/" >Cancel</Link>
+        </div>
+      </form>
     )
   }
 }
 
-export default connect(null, null)(EventsNew)
+const validate = values => {
+  const errors = {}
+
+  if (!values.title) errors.title = "Enter a title, please."
+  if (!values.body) errors.body = "Enter a body, please."
+
+  return errors
+}
+
+const mapDispatchToProps = ({ postEvent })
+
+export default connect(null, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
